3fcf7e90a0093e8d484b240340fc75806d5b0541
klmelchor <melchor.kyle@gmail.com>
2023-03-15 11:06:30 +0800

    Created accident_prediction script from experiments in notebook

---
+++ accident_prediction.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np
+import os
+
+from sklearn.linear_model import SGDRegressor
+from sklearn.preprocessing import StandardScaler
+
+
+# first we create the needed cleaned datasets for each category
+
+def create_datasets(df, acc='all'):
+    acc_to_use = ['Alkoholunfälle', 'Fluchtunfälle', 'Verkehrsunfälle'] if acc == 'all' else [acc]
+    df_subset = df[(df['MONATSZAHL'].isin(acc_to_use)) & (df['JAHR'] <= 2020) & (df['MONAT'] != 'Summe') 
+                & (df['AUSPRÄGUNG'] == 'insgesamt')].iloc[:,2:6].fillna(0)
+    df_subset = df_subset.rename(columns={'JAHR': 'Year', 'MONAT': 'Month', 'WERT': 'Total', 'VORJAHRESWERT': 'Prev_Year'})
+
+    # we use the Month_Clean column to process the test parameter
+    month_list = list(df_subset['Month'].str[-2:])
+    month_list_clean = [int(x) if int(x) >= 10 else int(x[-1:]) for x in month_list]
+    df_subset['Month_Clean'] = month_list_clean
+
+    df_subset.to_pickle(str(acc) + ".pkl")
+    return df_subset
+
+def data_initialization(df):
+    accident_cats = df['MONATSZAHL'].unique()
+    for acc in accident_cats:
+        file_path = str(acc) + '.pkl'
+        if os.path.isfile(file_path) == True:
+            print(file_path, 'exists, skipping...')
+            continue
+        else:
+            create_datasets(acc)
+
+def normalize_values(year_val, month_val, df_to_use):
+    month_val_conv = int(str(year_val) + str(month_val) if month_val >= 10 else str(year_val) + '0' + str(month_val))
+    
+    # find the value for the previous year, since this is one of the features needed by LinRes
+    prev_year_val = int(df_to_use[(df_to_use['Year'] == year_val - 1) & 
+                                    (df_to_use['Month_Clean'] == month_val)]['Total'])
+
+    year_val_norm = (year_val - df_to_use['Year'].mean()) / df_to_use['Year'].std()
+    prev_year_val_norm = (prev_year_val - df_to_use['Prev_Year'].mean()) / df_to_use['Prev_Year'].std()
+    month_val_conv_norm = (month_val_conv - pd.to_numeric(df_to_use['Month']).mean()) / pd.to_numeric(df_to_use['Month']).std()
+    
+    return year_val_norm, prev_year_val_norm, month_val_conv_norm
+
+def train(df_to_use):
+    X_features = ['Year', 'Month', 'Prev_year']
+    X_train = df_to_use.iloc[:, [0,1,3]]
+    y_train = df_to_use['Total']
+
+    scaler = StandardScaler()
+    X_norm = scaler.fit_transform(X_train)
+
+    sgdr = SGDRegressor(max_iter=1000)
+    sgdr.fit(X_norm, y_train)
+    print(f"number of iterations completed: {sgdr.n_iter_}, number of weight updates: {sgdr.t_}")
+
+    b_norm = sgdr.intercept_
+    w_norm = sgdr.coef_
+    print(f"model parameters:                   w: {w_norm}, b:{b_norm}")
+    
+    return sgdr, w_norm, b_norm
+
+
+
+def main_func(year, month):
+    df = pd.read_csv('monatszahlen2209_verkehrsunfaelle.csv')
+    
+    # initialize the datasets for alcohol, escape, and traffic
+    data_initialization(df)
+
+    # using alcohol for now
+    df_alcohol = pd.read_pickle('Alkoholunfälle.pkl')
+
+    year_val_norm, prev_year_val_norm, month_val_conv_norm = normalize_values(year, month, df_alcohol)
+    sgdr, w_norm, b_norm = train(df_alcohol)
+
+    y_pred_sgd = sgdr.predict([[year_val_norm, month_val_conv_norm, prev_year_val_norm]])
+    
+    return float(y_pred_sgd[0])
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import json
+import accident_prediction as acc_pred
 
 app = Flask(__name__)
 
@@ -10,7 +11,10 @@ def home():
 @app.route('/api/task', methods=['POST'])
 def get_query():
     query_data = request.get_json()
+    year = int(query_data['year'])
+    month = int(query_data['month'])
+    prediction = acc_pred.main_func(year, month)
     response = {
-        'prediction': query_data['year']
+        'prediction': prediction
     }
     return json.dumps(response)
