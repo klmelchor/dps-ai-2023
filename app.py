e611f60aadb15d5e59da2ecb78196ec2b20007a3
klmelchor <melchor.kyle@gmail.com>
2023-03-15 10:49:04 +0800

    Flask app creation with dummy POST evaluation

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello World"
+
+@app.route('/api/task', methods=['POST'])
+def get_query():
+    query_data = request.get_json()
+    response = {
+        'prediction': query_data['year']
+    }
+    return json.dumps(response)
